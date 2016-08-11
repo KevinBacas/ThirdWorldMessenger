084a66e0ed97369594590d3f18942871d392549d
kevinbacas <bacas.kevin@hotmail.fr>
2016-08-11 17:00:44 +0200

    Configure deps + 2 get api calls

--- src/index.js
+++ src/index.js
@@ -1,45 +1,90 @@
 const restify = require('restify');
-const DataStore = require('nedb');
-
-const DataBase = {
-  Messages: new DataStore({ filename: './Messages.db', autoload: true }),
-};
+const logger = require('restify-logger');
+const r = require('./utils/r');
 
 const server = restify.createServer({
   name: 'ThirdWorldMessenger',
 });
+logger.format('tiny', ':method :url :status :res[content-length] - :response-time ms');
+server.use(logger('tiny'));
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.jsonp());
 server.use(restify.bodyParser({ mapParams: true }));
 
 server.get('/', (req, res, next) => {
-  res.send('OK');
+  res.send({ status: 'OK' });
   return next();
 });
 
-server.get('/messages', (req, res, next) => {
-  DataBase.Messages.find({}, (err, messages) => {
-    if (err) return next(err);
-    res.send(messages);
-    return next();
-  });
+server.get('/ping', (req, res, next) => {
+  res.send('pong');
+  return next();
 });
 
-server.post('/messages', (req, res, next) => {
-  if (!req.params.sender || !req.params.message) {
-    return next(new restify.InvalidArgumentError("You must provide a sender and a message."));
+server.get('/thread', (req, res, next) => {
+  const authorization = req.header('Authorization', '');
+  if (authorization) {
+    r.table('ThreadUserXREF')
+    .filter({
+      FK_User_ID: authorization,
+    })
+    .eqJoin('FK_Thread_ID', r.table('Thread'))
+    .zip()
+    .merge(t => ({
+      messages: r.table('Message')
+                  .getAll(t('id'), { index: 'FK_Thread_ID' })
+                  .merge(m => ({
+                    sender: r.table('User')
+                              .get(m('FK_User_ID'))
+                  }))
+                  .coerceTo('array')
+    }))
+    .run()
+    .then(threads => {
+      res.send(threads);
+      return next(null);
+    })
+    .error(err => next(err));
+  } else {
+    return next(new restify.UnauthorizedError('You don\'t have access to this thread.'));
   }
-  const newMessage = {
-    sender: req.params.sender,
-    message: req.params.message,
-  };
-  DataBase.Messages.insert(newMessage, (err, createdMessage) => {
-    res.send(200, createdMessage);
-    return next();
-  });
 });
 
-const PORT = process.env.THRID_WORLD_PORT || 8080;
+server.get('/thread/:id', (req, res, next) => {
+  const id = req.params.id;
+  r.table('ThreadUserXREF')
+    .filter({
+      FK_User_ID: req.header('Authorization', ''),
+      FK_Thread_ID: id,
+    })
+    .count()
+    .run()
+    .then(c => {
+      if (c) {
+        r.table('Thread')
+          .get(id)
+          .merge(t => ({
+            messages: r.table('Message')
+                        .getAll(t('id'), { index: 'FK_Thread_ID' })
+                        .merge(m => ({
+                          sender: r.table('User')
+                                    .get(m('FK_User_ID'))
+                        }))
+                        .coerceTo('array')
+          }))
+          .then(thread => {
+            res.send(thread);
+            return next(null);
+          })
+          .error(err => next(err));
+      } else {
+        return next(new restify.UnauthorizedError('You don\'t have access to this thread.'));
+      }
+    })
+    .error(err => next(err));
+});
+
+const PORT = process.env.THIRD_WORLD_PORT || 8080;
 server.listen(PORT, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
